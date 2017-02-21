ff5bfca373d09c5e815922870dabfece0f17edbd
Jordana Approvato <jordana.approvato@gmail.com>
2017-02-20 23:50:47 -0500

    Works with input file now

--- largesum.cpp
+++ largesum.cpp
@@ -2,44 +2,47 @@
 #include <cstring>
 #include <stdio.h>
 #include <cstdlib>
+#include <fstream>
 using namespace std;
 
 #define toDigit(c) (c-'0')
 
 int main() {
-	int n = 100;
 	int linecount = 0;
 	string val;
 	int valLength = 0;
 	int maxLen = 50;
 	char numbers[200][50];
 	char sum[52];
-	
-	// TODO: add read from file code
-	// should be while no EOF
-	while(linecount < n){
-		cin >> val;
-		valLength = val.length();
-		
-		// moves string input value to a char array
-		// of size 50 with 
-		for(int i=0, j=0; i < maxLen; i++){
-			if(i < 50-valLength){
-				numbers[linecount][i]= '0';
+
+	ifstream myfile ("input.txt");
+	if(myfile.is_open()) {
+		while(getline(myfile,val)) {
+			valLength = val.length();
+
+			// moves string input value to a char array
+			// of size 50 with
+			for(int i=0, j=0; i < maxLen; i++){
+				if(i < 50-valLength){
+					numbers[linecount][i]= '0';
+				}
+				else{
+					numbers[linecount][i] = val[j++];
+				}
 			}
-			else{
-				numbers[linecount][i] = val[j++];
-			}
+			linecount++;
 		}
-		linecount++;
+		myfile.close();
+	} else {
+		cout << "Unable to open file";
 	}
-	
+
 	// addition using carry over method
 	string totalSum = "";
 	int smallSum = 0;
 	int carry = 0;
 	int currDigit;
-	
+
 	//iterate through digits 49 through 0
 	for(int i=maxLen-1; i >=0; i--){
 		smallSum = 0;
@@ -50,14 +53,14 @@ int main() {
 		smallSum += carry;
 		currDigit = smallSum % 10;
 		carry = (smallSum-currDigit)/10;
-		
+
 		if(i == 0){
 			totalSum = to_string(smallSum) + totalSum;
 		} else {
 			totalSum = to_string(currDigit) + totalSum;
 		}
 	}
-	
+
 	//remove '0' padding from totalSum
 	string firstTenDigits = "";
 	bool hasPadding = false;
@@ -70,11 +73,11 @@ int main() {
 			hasPadding = false;
 		}
 	}
-	
+
 	firstTenDigits = totalSum.substr(0,10);
-	
+
 	cout << "Full sum: " << totalSum << endl;
 	cout << "First 10 digits: " << firstTenDigits << endl;
-	
+
 	return 0;
 }
